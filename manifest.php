45fd1cba2f40e0249ebdbf1ed1f5421b57ffbe65
Ross Parler <rparker@MBP012-Ross-Parker.local>
2013-10-02 07:57:17 +0800

    Textarea Fix
    Fixed textarea width issue

--- manifest.php
+++ manifest.php
@@ -25,7 +25,7 @@ $description="A module to facilitate schools to run the IB Diploma programme." ;
 $entryURL="index.php" ;
 $type="Additional" ;
 $category="IB" ;
-$version="1.0.01" ;
+$version="1.0.02" ;
 $author="Ross Parker" ;
 $url="http://rossparker.org" ;
 
