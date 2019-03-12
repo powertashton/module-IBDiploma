5252f3306116d31283fb9fd5be5cb52015b1bd87
Ashton Power <powertashton@gmail.com>
2019-03-12 12:00:29 +0800

    Manifest Update from v1.0.11 to v1.1.00

--- IB Diploma/manifest.php
+++ IB Diploma/manifest.php
@@ -25,7 +25,7 @@ $description = 'A module to facilitate schools to run the IB Diploma programme.'
 $entryURL = 'index.php';
 $type = 'Additional';
 $category = 'IB';
-$version = '1.0.11';
+$version = '1.1.00';
 $author = 'Ross Parker, Ashton Power';
 $url = 'http://rossparker.org';
 
