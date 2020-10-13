b5f86d1f7a0c4dc99b0c3b2d8e11cabdb4bbb9ac
Ashton Power <48048360+powertashton@users.noreply.github.com>
2020-10-13 12:54:21 +0800

    CAS_adviseStudents Table refactoring (#5)
    * gateways and advise student table
    
    * Searchable Student Table
    
    * Update CASStudentGateway.php
    
    * Update IB Diploma/src/Domain/CommitmentGateway.php
    
    * Update IB Diploma/src/Domain/CASStudentGateway.php
    
    * Update IB Diploma/src/Domain/CASStudentGateway.php
    
    * Update cas_adviseStudents.php
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Create .gitignore

---
+++ IB Diploma/src/Domain/CASStudentGateway.php
@@ -0,0 +1,50 @@
+<?php
+namespace Gibbon\Module\IBDiploma\Domain;
+
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\QueryableGateway;
+
+/**
+ * CAS Student Gateway
+ *
+ * @version v21
+ * @since   v21
+ */
+class CASStudentGateway extends QueryableGateway
+{
+    use TableAware;
+
+    private static $tableName = 'ibDiplomaStudent';
+    private static $primaryKey = 'ibDiplomaStudentID';
+    private static $searchableColumns = ['surname', 'preferredName'];
+
+    
+    public function queryCASStudents($criteria, $gibbonSchoolYearID, $gibbonSchoolYearSequenceNumber, $gibbonPersonID) {      
+        $query = $this
+            ->newQuery()
+            ->from('ibDiplomaStudent')
+            ->cols(['gibbonPerson.gibbonPersonID', 'ibDiplomaStudentID', 'surname', 'preferredName', 'start.name AS start', 'end.name AS end', 'gibbonYearGroup.nameShort AS yearGroup', 'gibbonRollGroup.nameShort AS rollGroup', 'gibbonRollGroup.gibbonRollGroupID', 'gibbonPersonIDCASAdvisor', 'casStatusSchool'])
+            ->leftjoin('gibbonPerson', 'ibDiplomaStudent.gibbonPersonID=gibbonPerson.gibbonPersonID' )
+            ->leftjoin('gibbonStudentEnrolment', 'ibDiplomaStudent.gibbonPersonID=gibbonStudentEnrolment.gibbonPersonID')
+            ->leftJoin('gibbonSchoolYear AS start', 'start.gibbonSchoolYearID=ibDiplomaStudent.gibbonSchoolYearIDStart')
+            ->leftJoin('gibbonSchoolYear AS end','end.gibbonSchoolYearID=ibDiplomaStudent.gibbonSchoolYearIDEnd')
+            ->leftJoin('gibbonYearGroup','gibbonStudentEnrolment.gibbonYearGroupID=gibbonYearGroup.gibbonYearGroupID')
+            ->leftJoin('gibbonRollGroup', 'gibbonStudentEnrolment.gibbonRollGroupID=gibbonRollGroup.gibbonRollGroupID')
+            ->where('gibbonStudentEnrolment.gibbonSchoolYearID=:gibbonSchoolYearID ')->bindvalue('gibbonSchoolYearID', $gibbonSchoolYearID)
+            ->where('gibbonPerson.status="Full"')
+            ->where('start.sequenceNumber<=:sequenceStart')->bindvalue('sequenceStart', $gibbonSchoolYearSequenceNumber)
+            ->where('end.sequenceNumber>=:sequenceEnd')->bindvalue('sequenceEnd', $gibbonSchoolYearSequenceNumber);
+
+        $criteria->addFilterRules([
+            'gibbonRollGroupID' => function ($query, $gibbonRollGroupID) {
+                return $query
+                    ->where('gibbonRollGroup.gibbonRollGroupID=:gibbonRollGroupID')
+                    ->bindValue('gibbonRollGroupID', $gibbonRollGroupID);
+            }
+        ]);
+
+       return $this->runQuery($query, $criteria);
+    }
+}
+        
